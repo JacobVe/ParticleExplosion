247e7637165d0e960849b7713df50d891d33e0b3
Jacob <jacob.vermeer@outlook.com>
2018-10-26 18:36:43 +1030

    Final

--- ParticleExplosion/Particle.cpp
+++ ParticleExplosion/Particle.cpp
@@ -5,10 +5,19 @@
 
 Particle::Particle(): m_x(0), m_y(0)
 {
-	m_direction = (2 * M_PI * rand())/RAND_MAX;
-	m_speed = (0.0001 * rand())/RAND_MAX;
+	init();
 }
 
+void Particle::init()
+{
+	m_x = 0;
+	m_y = 0;
+
+	m_direction = (2 * M_PI * rand()) / RAND_MAX;
+	m_speed = (0.04 * rand()) / RAND_MAX;
+
+	m_speed *= m_speed;
+}
 
 Particle::~Particle()
 {
@@ -17,9 +26,17 @@ Particle::~Particle()
 
 void Particle::update(int interval)
 {
+	m_direction += interval*0.0004;
+
 	double xSpeed = m_speed * cos(m_direction);
 	double ySpeed = m_speed * sin(m_direction);
 
 	m_x += xSpeed * interval;
 	m_y += ySpeed * interval;
+
+	if (m_x < -1 || m_x > 1 || m_y < -1 || m_y > 1)
+		init();
+
+	if (rand() < RAND_MAX / 100)
+		init();
 }
